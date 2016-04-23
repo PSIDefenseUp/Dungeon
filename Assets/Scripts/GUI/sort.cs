d293bedc1fee6840aa39bf01dd5e3145421a0be5
KallRuz <tharri29@gmail.com>
2016-04-23 17:17:17 -0500

    merge trash

---
+++ Assets/Scripts/GUI/sort.cs
@@ -0,0 +1,17 @@
+﻿/*
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AlphaNumericSort : UnityEditor.BaseHierarchySort
+{
+  public override int Compare(GameObject lhs, GameObject rhs)
+  {
+    if (lhs == rhs) return 0;
+    if (lhs == null) return -1;
+    if (rhs == null) return 1;
+
+    return UnityEditor.EditorUtility.NaturalCompare(lhs.name, rhs.name);
+  }
+}
+*/
